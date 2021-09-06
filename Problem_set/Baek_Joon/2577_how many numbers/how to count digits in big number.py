631c09a1b5a897d129672909ca04f778714a6682
kongohan <countbox123@gmail.com>
2021-09-06 17:43:45 +0900

    how to count big digits

---
+++ Problem_set/Baek_Joon/2577_how many numbers/how to count digits in big number.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+len_T = 3
+T=1
+for numbers in range(3):
+    T = T*int(input())
+
+T_string=str(T)
+T_map = map(int,T_string)
+T_list = list(T_map)
+# print(T_list) #just checking for how does it works in the middle of code.
+
+for digits in range(0,9):
+    print((T_list.count(digits)))
