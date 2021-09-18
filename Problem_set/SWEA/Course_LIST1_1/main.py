7b7be2f6173f31e04b030dd83b5473c7a2560876
kongohan <countbox123@gmail.com>
2021-09-18 10:20:41 +0900

    change the codes of Elec. charge Prob

--- Problem_set/SWEA/Course_LIST1_1/main.py
+++ Problem_set/SWEA/Course_LIST1_1/main.py
@@ -12,7 +12,7 @@ for m in range(case_num): #각 test Case 별 구문
     fuel_tank = int(size_move) #Fuel tank는 버스의 남은 연료 표기
     pos_charge = list(map(int, input().split()))
     #Pos_charge는 정류장의 위치
-    charge_stop = [True]*(size_stop+1)
+    charge_stop = [False]*(size_stop+1)
     # Null list 를 만들줄 몰라서, 억지로 1by 10짜리 Array를 만든거임.
     # print(f'size_move : {size_move}') #표기용
     # print(f'size_stop : {size_stop}') #표기용
@@ -25,37 +25,67 @@ for m in range(case_num): #각 test Case 별 구문
     for k in range(size_stop+1):
         now_pos = now_pos + 1
         fuel_tank = fuel_tank - 1
-        # print(f'now we are in {k+1}')
-        print(f'now we have {fuel_tank} more gases')
-        determinant = []
+        if fuel_tank < 0:
+            # print(f'run out of gases')
+            break
 
-        if k+1 < size_stop and charge_stop[k+1] == True:
-            for temp_search in range(fuel_tank):
-                if charge_stop[k+1+temp_search+1] == True:
-                    determinant.append(1)
+        else:
+            # print(f'now we are in {now_pos}')
+            # print(f'now we have {fuel_tank} more gases')
+            determinant = []
+
+            if k+1 < size_stop and charge_stop[k+1] == True:
+                #1. 1칸 이동시마다,
+                #2. 현 이동 위치가, 정류소 갯수 이하만큼 왔는지 확인 (조건1)
+                #3. 해당 위치가 충전소인지 위치 확인 (조건2)
+                for temp_search in range(fuel_tank):
+                    #남은 연료만큼 더 가면 충전이 가능한지 체크위함
+                    # 남은 연료 2 일시, 0~1 출력
+                    if charge_stop[k+1+temp_search+1] == True:
+                        #k+1 : 현재 이동위치
+                        #temp_search+1 : 남은 연료 표시.
+                        # 조건문 : 현재 위치 기준으로 최대 이동 가능 거리 內 충전소 있는지 확인
+                        determinant.append(1) #충전소 있을시 Det = 1
+                    else:
+                        determinant.append(0) #충전소 없을시 Det = 0
+
+                # print(f'it is determinant {determinant}')
+                # print(f'it is sum of determinant {sum(determinant)}')
+
+                if sum(determinant)>0:
+                    #충전소 있을시 Det =1 이므로, 충전하지 않고 이동
+                    refill_gas = refill_gas
+                    fuel_tank = fuel_tank
+                    determinant = []
+                    # print(refill_gas)
                 else:
-                    determinant.append(0)
+                    # 충전소 없을시, 충전이 필요하나, 맨끝이 다가왔을때는 그냥 가도록 설정
+                    if fuel_tank >= ((size_stop)-now_pos):
+                    #현재 위치가 끝 충전소이고, 남은 연료로 충분히 갈 수 있다고 하면 충전 안함
+                        refill_gas = refill_gas
+                        fuel_tank = fuel_tank
+                        determinant = []
+                    else:
+                    # 현재 위치가 끝 충전소가 아니고, 남은 연료로 충분히 갈 수 없으면 충전
+                        refill_gas = refill_gas+1
+                        # print(f'Refilled gas')
+                        fuel_tank = size_move
+                        determinant = []
 
-            # print(f'it is determinant {determinant}')
-            # print(f'it is sumf of determinant {sum(determinant)}')
-
-            if sum(determinant)>0:
+            else:
                 refill_gas = refill_gas
                 fuel_tank = fuel_tank
                 determinant = []
                 # print(refill_gas)
-            else:
-                refill_gas = refill_gas + 1
-                print(f'Refilled gas')
-                fuel_tank = size_move
-                determinant = []
-                # print(refill_gas)
-        else:
-            refill_gas = refill_gas
-            fuel_tank = fuel_tank
-            determinant = []
-            # print(refill_gas)
 
-    print(refill_gas)
-    fuel_tank = size_move
-    refill_gas=0
+    mod_check = size_stop//size_move
+    if refill_gas < mod_check :
+        print(f'#{m+1} 0')
+        fuel_tank = size_move
+        refill_gas = 0
+        now_pos = 0
+    else:
+        print(f'#{m+1} {refill_gas}')
+        fuel_tank = size_move
+        refill_gas=0
+        now_pos=0
